a17574f2776cf3235bfa099c9707656787fa2e57
litrehinn <litrehinn@smail.nju.edu.cn>
2023-02-14 07:13:51 +0800

    fix core bug

--- am-apps/dummy/dummy.c
+++ am-apps/dummy/dummy.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
 #include <am.h>
 #include <klib-macros.h>
+const char str[] = "Exist with code";
 int main(){
-  int a=0;
-  int b=5;
-  int c=a+b;
-  printf("now year is %d\n", io_read(AM_TIMER_RTC).year);
-  while (a < 1000000) {
-    a++;
-    if (a%100000 == 0)
-    printf("now time is %d\n", io_read(AM_TIMER_UPTIME).us);
-  }
-  
-  printf("sgsgsg\n");
+  char str_tmp[50];
+  char *stt = str_tmp;
+  for (char *st = str; *st; st++) *stt++ = *st;
+  *stt++ = '\0';
+  putstr(str_tmp);
   return 0;
 }
